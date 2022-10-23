c4393112a39e575db2a46b7068d4453d5a2b5eb7
Vladimir <ermolenko.vm@dclouds.ru>
2022-10-23 03:51:32 +0300

    third revision

--- second.py
+++ second.py
@@ -10,8 +10,6 @@ with open('second_data.csv') as data_inp:
         '3.0': {'deals': 0, 'first_touches_num': 0, 'first_touches_list': {}, 'sum_deal_length': 0, 'last_date': datetime(1, 1, 1, 0, 0), 'manager_name': 'Joe Biden'}
     }
 
-    months = set()
-
     for index, row in enumerate(csv_data):
         manager_id = row[3]
         client_id = row[0]
@@ -29,7 +27,7 @@ with open('second_data.csv') as data_inp:
             prev_row = csv_data[index - 1]
             next_row = csv_data[index + 1]
 
-            if manager_id == prev_row[3] and prev_row[0] == client_id:
+            if manager_id == prev_row[3]:
                 date_first_touch = datetime.strptime(prev_row[1], '%Y-%m-%d')
                 delta = (date - date_first_touch).days
 
@@ -37,11 +35,20 @@ with open('second_data.csv') as data_inp:
                     managers[manager_id]['deals'] += 1
                     managers[manager_id]['sum_deal_length'] += delta
 
+                else:
+                    managers[manager_id]['first_touches_num'] -= 1
+                    managers[manager_id]['first_touches_list'].pop(client_id)
+            else:
+                managers[prev_row[3]]['first_touches_num'] -= 1
+                managers[prev_row[3]]['first_touches_list'].pop(client_id)
+
     for manager, values in managers.items():
         avg_deal_time = round(values['sum_deal_length'] / values['deals'])
 
-        for client_id, date_first_touch in values['first_touches_list'].items():
+        for client_id, date_first_touch in list(values['first_touches_list'].items()):
             if (values['last_date'] - date_first_touch).days < avg_deal_time:
                 values['first_touches_num'] -= 1
+                values['first_touches_list'].pop(client_id)
 
-        print(values['manager_name'] + ': ' + str(round(values['deals'] / values['first_touches_num'] * 100, 2)) + ' %')
+        conversion = str(round(values['deals'] / (values['first_touches_num'] + values['deals']) * 100, 2))
+        print(values['manager_name'] + ': ' + conversion + ' %')
